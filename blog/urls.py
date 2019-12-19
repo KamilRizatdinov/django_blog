b9933dc48a154be14fe024627eab454edf26e31f
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-19 22:02:14 +0300

    Add new views for blog app: blog:detail and blog:read

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
+    path('read/', views.read, name='read'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from blog.models import Post
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from Django App!")
+    latest_posts_list = Post.objects.order_by('-publication_date')[:5]
+    context = {'latest_posts_list': latest_posts_list}
+    return render(request, 'blog/index.html', context)
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
+
+
+def read(request):
+    post = Post.objects.get(pk=request.POST['post_id'])
+    post.views += 1
+    post.save()
+    return HttpResponseRedirect(reverse('blog:index'))
