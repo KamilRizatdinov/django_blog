55401dd5cb160896c634de789f74537d27b59560
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-19 18:09:14 +0300

    Add redirection from blank endpoint into the ../blog

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/blog/')),
     path('blog/', include('blog.urls')),
     path('contributors/', include('contributors.urls')),
     path('admin/', admin.site.urls),
