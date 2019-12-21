a85833ac6c0adf304da3e3c1a87db9225f1b58fa
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-22 00:52:59 +0300

    Add view for contributors:index

--- contributors/views.py
+++ contributors/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Here will be list of contributors to this project")
+    r = requests.get("https://api.github.com/repos/aiogram/aiogram/stats/contributors")
+    json_data = json.loads(r.text)
+    contributors = [
+        {'total': contributor['total'],
+         'login': contributor['author']['login'],
+         'avatar': contributor['author']['avatar_url']}
+        for contributor in json_data
+    ]
+    context = {'contributors': contributors}
+    return render(request, 'contributors/index.html', context)
