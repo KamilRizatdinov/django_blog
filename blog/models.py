5e7f8abcb34d1b54d07c81f8a30b392d5ca6ddee
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-22 00:14:35 +0300

    Add __str__ implementation for Post model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     text = models.CharField(max_length=1000, default='Empty Text')
     views = models.IntegerField(default=0)
     publication_date = models.DateField()
+
+    def __str__(self):
+        return self.header
