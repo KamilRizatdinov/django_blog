3c28d6ab196e39e994aa8cdddd0f37bebd453623
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-22 00:15:18 +0300

    Add Contributor model

--- contributors/admin.py
+++ contributors/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from contributors.models import Contributor
 
 # Register your models here.
+admin.site.register(Contributor)
--- contributors/models.py
+++ contributors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Contributor(models.Model):
+    commits = models.IntegerField(default=0)
+    login = models.CharField(max_length=100, default='Stranger')
+
+    def __str__(self):
+        return self.login
