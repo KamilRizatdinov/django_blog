31827d8f21db72d34a25ef63f31f217095414c20
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-25 19:50:10 +0300

    Add test_no_posts into the blog:index testing

--- blog/tests.py
+++ blog/tests.py
@@ -5,5 +5,17 @@ from django.shortcuts import reverse
 # Create your tests here.
 class BlogIndexViewTests(TestCase):
     def test_status_code(self):
+        """
+        If the page is loaded, response contains appropriate status code.
+        """
         response = self.client.get(reverse('blog:index'))
         self.assertEqual(response.status_code, 200)
+
+    def test_no_posts(self):
+        """
+        If no posts exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('blog:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No posts are available')
+        self.assertQuerysetEqual(response.context['latest_posts_list'], [])
