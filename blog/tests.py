d694c5ff16330eb3aaba3b0e4bd03888a230073b
KamilRizatdinov <k.rizatdinov@innopolis.university>
2019-12-25 19:38:36 +0300

    Add test_status_code into the blog:index testing

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+
 
 # Create your tests here.
+class BlogIndexViewTests(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse('blog:index'))
+        self.assertEqual(response.status_code, 200)
